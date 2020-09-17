d1d3fae1abdee306663c2bab0d66d46d9828b07f
exuberant-object-2894 <semillavelasco01@gmail.com>
2020-09-17 19:58:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
-// write your code below!
-let name=’you’;
-let holiday=’Independence Day’;
-let number=20;
-let mother=”Mother’s Day”;
-
-function happyHolidays(){
-return “Happy holidays!”;
+function happyHolidays() {
+  return "Happy holidays!"
 }
-happyHolidays();
 
 function happyHolidaysTo(name){
-return `Happy holidays, ${name}!`;
+  return `Happy holidays, ${name}!`
 }
-happyHolidaysTo();
 
 function happyHolidayTo(holiday, name){
-return `Happy ${holiday}, ${name}!`;
+  return `Happy ${holiday}, ${name}!`
 }
-happyHolidayTo();
 
-function holidayCountdown(mother, number){
-return `It’s ${number} days until ${mother}!`;
+function holidayCountdown(holiday, days){
+  return `It\'s ${days} days until ${holiday}!`
 }
-holidayCountdown();
