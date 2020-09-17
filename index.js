b175028e89da9b825deaba7da35a5f05912015c8
exuberant-object-2894 <semillavelasco01@gmail.com>
2020-09-17 20:08:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyCustomHolidaysTo(name){
+function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
 
-function happyHolidayTo(holiday, name){
+function happyCustomHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
 
